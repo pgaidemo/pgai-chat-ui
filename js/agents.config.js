9c9d496c49cb062d0bc64c0d413352f62922c35e
Ravi Tanguturi <tanguturi@me.com>
2026-02-09 14:17:59 -0500

    Recommit

--- js/agents.config.js
+++ js/agents.config.js
@@ -35,7 +35,7 @@ export const AGENTS = {
     prompts: {
       billing: {
         label: "Billing issue",
-        text: `Hello Support Team I am contacting you regarding a billing issue on my account The credit card I used was AmEx 3782 822463 10005`
+        text: `Hello Support Team I am contacting you regarding a billing issue on my account The credit card I used is AmEx 3782 822463 10005`
       },
 
       identity: {
